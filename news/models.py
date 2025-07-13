248294641b070010458a565b8ae805ffc170234d
minaZaeri2002 <minazaeri2002@gmail.com>
2025-07-13 12:49:20 +0330

    feat: add News, Tag and Source models

--- news/models.py
+++ news/models.py
@@ -1,2 +1,43 @@
 from django.db import models
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=200)
+    website = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    source = models.ForeignKey(
+        Source,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    tags = models.ManyToManyField(Tag, blank=True)
+    external_url = models.URLField(blank=True)
+    published_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_short_content(self, length=100):
+        if len(self.content) > length:
+            return self.content[:length] + '...'
+        return self.content
