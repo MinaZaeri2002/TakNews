65e5cec3148d1439cb90652644bb5625b8b45803
minaZaeri2002 <minazaeri2002@gmail.com>
2025-07-13 12:58:32 +0330

    feat: register admin, news app and configure REST framework

--- TakNews/settings.py
+++ TakNews/settings.py
@@ -14,11 +14,16 @@ DEBUG = os.getenv("DEBUG")
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'news'
 ]
 
 MIDDLEWARE = [
