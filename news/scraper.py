e4f3cde061439f5a624ffa13798a421fff03eb21
minaZaeri2002 <minazaeri2002@gmail.com>
2025-07-22 18:07:36 +0330

    run spider in separate process to avoid Twisted restart error

--- news/scraper.py
+++ news/scraper.py
@@ -1,9 +1,21 @@
+from billiard import Process
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scraper.spiders.zoomit_spider import ZoomitSpider
 
 
-def scrape_news():
+def run_spider():
     process = CrawlerProcess(get_project_settings())
     process.crawl(ZoomitSpider)
     process.start()
+
+
+def scrape_news():
+    p = Process(target=run_spider)
+    p.start()
+    p.join()
+
+    if p.exitcode == 0:
+        print("Scraping completed successfully")
+    else:
+        print(f"Scraping failed with exit code: {p.exitcode}")
